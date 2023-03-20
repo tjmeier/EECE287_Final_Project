e4db8eaf85736ec9c5ca5cc1c9bf6eaa41fc6b74
Tyler Meier <tmeier1@binghamton.edu>
2023-03-20 11:41:45 -0400

    create functions for motor pwm and direction

--- motor_speed_control_pwm.c
+++ motor_speed_control_pwm.c
@@ -6,64 +6,81 @@
 #include <util/delay.h> 
 #include <stdint.h>
 #include <stdbool.h>
+#include "zumo_drivers.h"
 
-#define A_BUTTON_LOCATION 3
-#define YELLOW_LED_LOCATION 7
-
-//replace X with the pin mapping for button C
-#define C_BUTTON_LOCATION 0
+//Output Port B
+#define LEFT_MOTOR_PWM_LOCATION 6
+#define RIGHT_MOTOR_PWM_LOCATION 5
+#define LEFT_MOTOR_DIRECTION_LOCATION 2
+#define RIGHT_MOTOR_DIRECTION_LOCATION 1
 
 //define PWM parameters
 #define PWM_TOP 100 //maximum value of 100, indicating 100%
-#define INCREMENT 5 //how much the duty cycle changes with each button press 
 
-void configure_A_button(){
-	
-	DDRB &= ~(1<< A_BUTTON_LOCATION); 
-	PORTB |= (1<< A_BUTTON_LOCATION); 
+
+void configure_motors(){
+
+	//Sets DDRB to be outputs at the 4 motor locations	
+	DDRB |= (1<< LEFT_MOTOR_PWM_LOCATION); 		
+	DDRB |= (1<< RIGHT_MOTOR_PWM_LOCATION); 
+	DDRB |= (1<< LEFT_MOTOR_DIRECTION_LOCATION); 		
+	DDRB |= (1<< RIGHT_MOTOR_DIRECTION_LOCATION);
+
+	//Clears all PORTB at motor locations
+	PORTB &= ~(1<< LEFT_MOTOR_PWM_LOCATION); 		
+	PORTB &= ~(1<< RIGHT_MOTOR_PWM_LOCATION); 
+	PORTB &= ~(1<< LEFT_MOTOR_DIRECTION_LOCATION); 		
+	PORTB &= ~(1<< RIGHT_MOTOR_DIRECTION_LOCATION); 		
 }
 
-void configure_yellow_led()
-{
-	DDRC |= (1<<YELLOW_LED_LOCATION);
+////Toggling motor pwms////
+void turn_on_left_motor_pwm(){
+	PORTB |= (1<< LEFT_MOTOR_PWM_LOCATION); 
 }
 
-void turn_on_yellow_led()
-{
-	PORTC |= (1<<YELLOW_LED_LOCATION);
+void turn_off_left_motor_pwm(){
+	PORTB &= ~(1<< LEFT_MOTOR_PWM_LOCATION); 
 }
 
-void turn_off_yellow_led()
-{
-	PORTC &= ~(1<<YELLOW_LED_LOCATION);
+void turn_on_left_motor_pwm(){
+	PORTB |= (1<< LEFT_MOTOR_PWM_LOCATION); 
 }
 
-
-//write code below to configure and set up button C 
-
-void configure_C_button(){
-	
-	DDRB &= ~(1<< C_BUTTON_LOCATION); 
-	PORTB |= (1<< C_BUTTON_LOCATION);		//it should start at 1?
+void turn_off_left_motor_pwm(){
+	PORTB &= ~(1<< LEFT_MOTOR_PWM_LOCATION); 
 }
 
+////Toggling motor directions////
+void set_reverse_left_motor_direction(){
+	//setting the port = reverse
+	PORTB |= (1<< LEFT_MOTOR_DIRECTION_LOCATION); 
+}
+
+void set_forward_left_motor_direction(){
+	//clearing the port = forward
+	PORTB &= ~(1<< LEFT_MOTOR_DIRECTION_LOCATION); 
+}
+
+void set_reverse_right_motor_direction(){
+	//setting the port = reverse
+	PORTB |= (1<< RIGHT_MOTOR_DIRECTION_LOCATION); 
+}
+
+void set_forward_right_motor_direction(){
+	//clearing the port = forward
+	PORTB &= ~(1<< RIGHT_MOTOR_DIRECTION_LOCATION); 
+}
 
 int main() { 
 
-	//configure ports
-	configure_yellow_led();
-	configure_A_button();
-	configure_C_button();
+    //from zumo_drivers.h
+    configure_zumo();
 
-	//add code to configure button C 
-	unsigned int last_button_C_state = (PINB & (1 << C_BUTTON_LOCATION));
-	unsigned int button_C_pressed;
 
 	//initialize variables
- 	unsigned int pwm_counter=0;    	
-	unsigned int duty_cycle=30;   
-	unsigned int last_button_A_state = (PINB & (1 << A_BUTTON_LOCATION));
-	unsigned int button_A_pressed;
+ 	unsigned int pwm_counter = 0;    	
+	unsigned int duty_cycle = 0;
+	unsigned int left_motor_speed = 0, right_motor_speed = 0;
 
 
 
@@ -74,10 +91,19 @@ int main() {
  	 	//PWM Counter 
  	 	pwm_counter = pwm_counter + 1; 
  	 	if( pwm_counter >= PWM_TOP ){ 
- 	 	 	pwm_counter = 0; 
+ 	 	 	pwm_counter = 0; //once the counter reaches the top, resets to zero
  	 	} 
   
- 	 	//PWM on yellow LED  	 	
+ 	 	//PWM for Left Motor
+		if (pwm_counter < duty_cycle) { 
+ 	 	 	turn_on_yellow_led();
+ 	 	}
+ 	 	else { 
+ 	 	 	turn_off_yellow_led();
+	 	} 
+
+		  
+ 		//PWM for Right Motor
 		if (pwm_counter < duty_cycle) { 
  	 	 	turn_on_yellow_led();
  	 	}
